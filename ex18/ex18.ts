022382eaad32cddd89fa91d53f4e5df4e9084be6
2uynhi <nhilhq234111e@st.uel.edu.vn>
2026-01-30 00:02:58 +0700

    Add files via upload

---
+++ ex18/ex18.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex18',
+  standalone: false,
+  templateUrl: './ex18.html',
+  styleUrl: './ex18.css',
+})
+export class Ex18 {
+
+}
